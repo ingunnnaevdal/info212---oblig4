ada0386dbd8a030c6a11ca16f70b8510b82ddba8
Ingunn <ina018@uib.no>
2022-10-17 11:51:37 +0200

    prøver å legge til customer og employee

--- Django/mysite/models.py
+++ Django/mysite/models.py
@@ -1,12 +1,33 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Car(models.Model):
     make = models.CharField(max_length= 50)
     carmodel = models.CharField(max_length= 50)
-
+    year = models.PositiveIntegerField(max_length = 4)
+    location = models.CharField(max_length = 50)
+    status = models.BooleanField(default = True)
 
     def __str__(self):
-        return f"{self.make} {self.carmodel}"
+        return f"{self.make} {self.carmodel} {self.year} {self.location} {self.status}"
+
+class Employee(models.Model):
+    name = models.CharField(max_length= 50)
+    address = models.CharField(max_length= 50)
+    branch = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return f"{self.name} {self.address} {self.branch}"
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length= 50)
+    address = models.CharField(max_length= 50)
+    age = models.PositiveIntegerField(max_length = 3)
+
+    def __str__(self):
+        return f"{self.name} {self.address} {self.age}"
+    
 
     # make and model are attributes where we can store strings.
     # The __str__ method just tells Django what to print when it
